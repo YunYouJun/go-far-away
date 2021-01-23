b654edd82966ab8c5512d1d2ae7ef0b8f98fc6f0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-23 13:10:21 +0000

    Deploy YunYouJun/go-far-away to github.com/YunYouJun/go-far-away.git:gh-pages

---
+++ precache-manifest.67c9952bc46ec88a253bc6686d89b885.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1f790af5dab516e70f",
+    "url": "/go-far-away/css/app.80a9c934.css"
+  },
+  {
+    "revision": "9ea974fe0619f3fc7d9d",
+    "url": "/go-far-away/css/chunk-vendors.00a14a73.css"
+  },
+  {
+    "revision": "132d988a8f57f7e1741a70d9cef7f1a2",
+    "url": "/go-far-away/img/logo.132d988a.svg"
+  },
+  {
+    "revision": "d8abd29f4df14a4cd4ee82a37ede2b31",
+    "url": "/go-far-away/img/logo.png"
+  },
+  {
+    "revision": "0bc27489b4ffd61e0c835f6e419fec87",
+    "url": "/go-far-away/index.html"
+  },
+  {
+    "revision": "9e1f790af5dab516e70f",
+    "url": "/go-far-away/js/app.cb3bd94e.js"
+  },
+  {
+    "revision": "9ea974fe0619f3fc7d9d",
+    "url": "/go-far-away/js/chunk-vendors.45916aa7.js"
+  },
+  {
+    "revision": "3cd8bd98a7a6f9f247b76eb21641c761",
+    "url": "/go-far-away/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/go-far-away/robots.txt"
+  }
+]);
